795cba3d03f632f77da92881d04d43985dd95e03
Alireza Sheikhi <Sheikhi.L.Alireza@gmail.com>
2024-11-09 20:29:12 +0330

    define variables and call functions

--- Section 2/Demo1/assets/scripts/app.js
+++ Section 2/Demo1/assets/scripts/app.js
@@ -1,1 +1,7 @@
-alert("This Works!!!")
+const defaultResult = 0;
+let currentResult = defaultResult;
+let calculationDescription = "(" + defaultResult + " + 10) * 3 / 2 -1";
+
+currentResult = (currentResult + 10) * 3 / 2 -1;
+
+outputResult(currentResult,calculationDescription);
