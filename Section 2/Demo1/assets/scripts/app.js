898e35f8741426912ba82b4a4ab0f1ad53f60963
Alireza Sheikhi <Sheikhi.L.Alireza@gmail.com>
2024-11-09 21:04:09 +0330

    added addition function

--- Section 2/Demo1/assets/scripts/app.js
+++ Section 2/Demo1/assets/scripts/app.js
@@ -2,6 +2,10 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
 
-currentResult = (currentResult + 10) * 3 / 2 -1;
+currentResult = add(1,2);
 
-outputResult(currentResult,calculationDescription);
+outputResult(currentResult, calculationDescription);
+
+function add(num1, num2) {
+    return num1 + num2;
+}
