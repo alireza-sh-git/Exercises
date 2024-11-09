b1ad63c7877a1ebc05cc051474a9171ca85cafed
Alireza Sheikhi <Sheikhi.L.Alireza@gmail.com>
2024-11-09 20:48:25 +0330

    changed string decleration to template literal

--- Section 2/Demo1/assets/scripts/app.js
+++ Section 2/Demo1/assets/scripts/app.js
@@ -1,6 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-let calculationDescription = "(" + defaultResult + " + 10) * 3 / 2 -1";
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
 
 currentResult = (currentResult + 10) * 3 / 2 -1;
 
